b12078aeabb9832cd7550b1d01c251291dbe8961
furry33 <60417722+jokeryy5@users.noreply.github.com>
2022-11-17 10:14:58 +0630

    Update README.cpp

--- README.cpp
+++ README.cpp
@@ -2,39 +2,39 @@
 #include "stdlib.h"
 
 int main(){
+    //declaritaion variable
     int age=0;
-
-    int record= 0;
-    int wrongKey= 0;
     int key= 0;
+
+    //intro game
     while(true){
         printf("This is happy world game!!!! \n");
         printf("Enter your age \n");   //age
         scanf("%d", &age);          //age
         printf("Your age is: %d \n", age);
 
+     //age limitation
         if(age>= 18){
             printf("You can play game!! \n");
-
-
-
-            for( int i= 0; i<5; i ++){
+    //account ask
+            while(){
                 printf("have already account?? press 1 \n");
                 printf("register?? press 2 \n");
                 scanf("%d", &key);
+    //account check
                 if(key==1){
                     printf("Enter your account \n");
-                    exit(1);
+                    continue;
                 }else if( key== 2){
                     printf("This is registration \n");
-                    exit(1);
+                    continue;
 
                 }else {
 
                     printf("Choose 1 or 2 \n"); //????????
-                   i=+i;
-                }
 
+                    continue;
+                }
             }
             printf("The form is still working:3");
 
